5b4234d9750491a356842481f7f81915ffc2f850
Andre.Stielau <andre.stielau@truphone.com>
2022-05-10 17:16:45 +0100

    Add Next SSR

---
+++ src/server/main.ts
@@ -0,0 +1,17 @@
+import { PORT } from 'src/shared/constants/env'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+declare const module: any
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule.initialize())
+  await app.listen(PORT)
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => app.close())
+  }
+}
+
+bootstrap()
