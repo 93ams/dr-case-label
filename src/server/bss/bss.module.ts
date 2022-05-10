b7b716fa3d5f19b025b1f6bb453ab4b4384eb37e
Andre.Stielau <andre.stielau@truphone.com>
2022-05-10 21:40:38 +0100

    missing label import

--- src/server/bss/bss.module.ts
+++ src/server/bss/bss.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common'
 import { EHRModule } from './ehr/ehr.module'
+import { LabelModule } from './ehr/label/label.module'
 import { ConditionModule } from './cond/condition.module'
 
 @Module({
-  imports: [ConditionModule, EHRModule],
+  imports: [ConditionModule, EHRModule, LabelModule],
 })
 export class BssModule {}
