cf0e6e5fb8da4ab4e425513fe3784277d2a5f000
Andre.Stielau <andre.stielau@truphone.com>
2022-05-12 17:00:50 +0100

    Cleanup app module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,12 +1,10 @@
 import { AppController } from './app.controller'
 import { CoreModule } from './core/core.module'
 import { BssModule } from './bss/bss.module'
-import { AppService } from './app.service'
 import { Module } from '@nestjs/common'
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
   imports: [CoreModule, BssModule],
 })
 export class AppModule {}
