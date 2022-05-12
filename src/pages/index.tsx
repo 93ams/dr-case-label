0385a857441fcda8f72e85e70dba187f091fa3b1
Andre.Stielau <andre.stielau@truphone.com>
2022-05-12 17:06:59 +0100

    index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
 import { FC } from 'react'
-import { Typography } from '@mui/material'
+import MainLayout from '../client/component/layout/main'
+import { Grid, TextField, Typography } from '@mui/material'
 
 const Home: FC = () => {
-  return <Typography>Home is where the heart is</Typography>
+  return (
+    <MainLayout>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Typography sx={{ marginBottom: '15px', marginLeft: '5px' }}>
+            Please Review This Case
+          </Typography>
+          <TextField sx={{ width: '100%' }} multiline rows={30} disabled />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography sx={{ marginBottom: '15px', marginLeft: '5px' }}>
+            Select Condition
+          </Typography>
+        </Grid>
+      </Grid>
+    </MainLayout>
+  )
 }
 
 export default Home
