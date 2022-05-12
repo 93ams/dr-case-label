c244e174f946c5217db9e50a300b21ed0eb916b8
Andre.Stielau <andre.stielau@truphone.com>
2022-05-12 17:05:51 +0100

    graphql and auth providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
-import { FC } from 'react'
-import { AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import createEmotionCache from '../client/theme/emotion'
-import { EmotionCache } from '@emotion/cache'
+import { ThemeProvider } from '../client/theme/provider'
+import { AuthProvider } from '../client/provider/auth'
 import { CacheProvider } from '@emotion/react'
+import { EmotionCache } from '@emotion/cache'
+import { AppProps } from 'next/app'
+import theme from '../client/theme'
+import { FC } from 'react'
 
 const clientSideEmotionCache = createEmotionCache()
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache
 }
+
+const client = new ApolloClient({
+  uri: 'https://localhost:3000/graphql',
+  cache: new InMemoryCache(),
+})
 
 const App: FC<MyAppProps> = ({
   Component,
@@ -17,7 +26,13 @@ const App: FC<MyAppProps> = ({
 }) => {
   return (
     <CacheProvider value={emotionCache}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <ApolloProvider client={client}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </AuthProvider>
+      </ThemeProvider>
     </CacheProvider>
   )
 }
