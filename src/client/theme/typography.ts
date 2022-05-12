dd4a1a246fed9ae2388bba94cc53c4b58387e6fd
Andre.Stielau <andre.stielau@truphone.com>
2022-05-12 17:03:20 +0100

    add lettering

---
+++ src/client/theme/typography.ts
@@ -0,0 +1,113 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography'
+
+const typography: TypographyOptions = {
+  fontFamily: 'SFProText, Helvetica, Arial',
+
+  h1: {
+    fontFamily: 'SFProText',
+    fontSize: '96px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  h2: {
+    fontFamily: 'SFProText',
+    fontSize: '60px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  h3: {
+    fontFamily: 'SFProText',
+    fontSize: '44px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  h4: {
+    fontFamily: 'SFProText',
+    fontSize: '28px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  h5: {
+    fontFamily: 'SFProText',
+    fontSize: '24px',
+    fontWeight: 400,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  h6: {
+    fontFamily: 'SFProText',
+    fontSize: '20px',
+    fontWeight: 500,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  subtitle1: {
+    fontFamily: 'SFProText',
+    fontSize: '16px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  subtitle2: {
+    fontFamily: 'SFProText',
+    fontSize: '14px',
+    fontWeight: 500,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  body1: {
+    fontFamily: 'SFProText',
+    fontSize: '18px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    lineHeight: 1.375,
+  },
+
+  body2: {
+    fontFamily: 'SFProText',
+    fontSize: '14px',
+    fontWeight: 'normal',
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  caption: {
+    fontFamily: 'SFProText',
+    fontSize: '12px',
+    fontWeight: 400,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  overline: {
+    fontFamily: 'SFProText',
+    fontSize: '14px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+  },
+
+  button: {
+    fontFamily: 'SFProText',
+    fontSize: '14px',
+    fontWeight: 700,
+    fontStretch: 'normal',
+    fontStyle: 'normal',
+    textAlign: 'center',
+  },
+}
+
+export default typography
