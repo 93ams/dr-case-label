5410f34564f927b9ebb9e72ef87d3798b67ef38a
Andre.Stielau <andre.stielau@truphone.com>
2022-05-12 17:01:28 +0100

    cleanup core

--- src/server/core/core.module.ts
+++ src/server/core/core.module.ts
@@ -4,8 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { AuthModule } from './auth/auth.module'
 import { CaslModule } from './casl/casl.module'
 import { GraphQLModule } from '@nestjs/graphql'
-import { Module } from '@nestjs/common'
 import { RenderModule } from 'nest-next'
+import { Module } from '@nestjs/common'
 import { join } from 'path'
 import Next from 'next'
 
@@ -19,12 +19,12 @@ import Next from 'next'
         dev: process.env.NODE_ENV !== 'production',
         dir: join(__dirname, '../..'),
       }),
-      { passthrough404: true, viewsDir: null },
+      { viewsDir: null },
     ),
     MongooseModule.forRoot('mongodb://localhost/nest'),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/shared/schema.gql'),
+      autoSchemaFile: join(process.cwd(), 'src/shared/graphql/schema.gql'),
     }),
   ],
 })
