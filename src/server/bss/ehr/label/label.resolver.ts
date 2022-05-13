2bde56399340a9a80a8b91b5bded76859989db25
Andre.Stielau <andre.stielau@truphone.com>
2022-05-13 15:52:21 +0100

    remove label and server redirects

--- src/server/bss/ehr/label/label.resolver.ts
+++
@@ -1,8 +0,0 @@
-import { Label } from '../../../../shared/graphql/label.model'
-import { Args, Int, Query, Resolver } from '@nestjs/graphql'
-import { LabelsService } from './label.service'
-
-@Resolver(() => Label)
-export class LabelsResolver {
-  constructor(private LabelsService: LabelsService) {}
-}
